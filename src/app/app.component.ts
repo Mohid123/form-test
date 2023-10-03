838ed73f4ccdd78e3e69a8fd3fd8eb4eb7851d3c
Mohid123 <muhammadmohid141@gmail.com>
2023-10-03 11:07:24 +0500

    form builder + renderer demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
+import { FormioRefreshValue } from '@formio/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'form-test';
+  public form: any = {
+    "title": "Test Form",
+    "key": "tf-a",
+    "display": "form",
+    "components": [
+      {
+        "label": "Text Field",
+        "tableView": true,
+        "key": "textField",
+        "type": "textfield",
+        "input": true
+      }
+    ]
+  };
+  public refreshForm: EventEmitter<FormioRefreshValue> = new EventEmitter();
+  formValue: any
+
+  onChange(event: any) {
+    this.refreshForm.emit({
+      property: 'form',
+      value: event.form
+    });
+    this.formValue = event.form;
+  }
 }
